3ad690a81aa5c7112af2a8917c7b051910cfbc44
InGodWeTrustt <fanat2016@mail.ru>
2022-11-03 00:22:39 +0300

    update

--- src/component/section.comp.js
+++ src/component/section.comp.js
@@ -8,7 +8,13 @@ export default function Section() {
             <section className="catalog">
                 <h2 className="visually-hidden">Список треков</h2>
                 <div className="catalog__container">
-                   <Card></Card>
+                    <Card></Card>
+                    <button className="catalog__btn-add">
+                        <span>Увидеть все</span>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8.59 16.59L13.17 12L8.59 7.41L10 6L16 12L10 18L8.59 16.59Z" />
+                        </svg>
+                    </button>
                 </div>
             </section>
 
