7ba6877493271d151c250f7ce80d6a2da12ba9ab
InGodWeTrustt <fanat2016@mail.ru>
2022-11-03 23:21:25 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].bundle.js',
         clean: true
     },
     module: {
