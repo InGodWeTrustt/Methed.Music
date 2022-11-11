a7d901ce48bd01eec48b45c130c253ae37d10d5f
InGodWeTrustt <fanat2016@mail.ru>
2022-11-11 14:00:14 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,28 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: [
+                    {
+                    loader: 'image-webpack-loader',
+                    options: {
+                        mozjpeg: {
+                            progressive: true,
+                        },
+                        optipng: {
+                            enabled: false,
+                        },
+                        pngquant: {
+                            quality: [0.65, 0.90],
+                            speed: 4
+                        },
+                        gifsicle: {
+                            interlaced: false,
+                        },
+                        webp: {
+                            quality: 75
+                        }
+                    }
+                }],
                 type: 'asset/resource',
                 generator: {
                     filename: 'assets/image/[name][ext]'
@@ -46,6 +68,7 @@ module.exports = {
             },
             {
                 test: /\.(mp3|wav)$/,
+                use: 'file-loader',
                 type: 'asset/resource',
                 generator: {
                     filename: 'assets/audio/[name][ext]'
